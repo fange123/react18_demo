9f7702de2954001e0772b2eefc85e2fea7660fcf
fange123 <2030564240@qq.com>
2022-06-28 11:09:08 +0800

    feat:汉堡项目准备

---
+++ hanbao/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+//* 设置移动端的适配
+document.documentElement.style.fontSize = 100 / 750 + "vw"; //指定视口的宽度
+//除以机，适口的宽度就是多少--750
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
