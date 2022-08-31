cb1df3a75ce0a65f10e82e25bf6df66958548369
fange123 <2030564240@qq.com>
2022-08-31 11:32:52 +0800

    feat:使用本地存储数据

--- react-app/src/store/reducer/authSlice.js
+++ react-app/src/store/reducer/authSlice.js
@@ -2,11 +2,22 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const authSlice = createSlice({
   name: 'auth',
-  initialState:{
-    isLogged: false,
-    token:'',
-    user:null
-  },
+  initialState:()=> {
+    //* 初始化登录数据的时候可以先去本地存储里看看有没有，没有再使用初始值
+  const token = localStorage.getItem('token')
+  if(!token) {
+      return {
+      isLogged: false,
+      token:'',
+      user:null
+    }
+  }
+  return {
+      isLogged: true,
+      token:token,
+      user:JSON.parse(localStorage.getItem('user'))
+    }
+},
   reducers:{
     //* 往redux里面存数据
     login(state,action){
@@ -14,11 +25,19 @@ export const authSlice = createSlice({
       state.token = action.payload.token
       state.user = action.payload.user
 
+      //# 本地存储一份用户信息
+      localStorage.setItem('token',state.token)
+      localStorage.setItem('user',JSON.stringify(state.user))
+
     },
     logout(state){
       state.isLogged = false
       state.token = ''
       state.user = null
+
+      //# 清空本地存储
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
     }
   }
 })
